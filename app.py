dac20932920c75c80adb73e2d708efd0f07075d6
Raghav Mittal <raghavmittal101@gmail.com>
2020-12-19 18:29:58 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request, jsonify
+from flask_pymongo import PyMongo
+import re
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, DecimalField, BooleanField, SubmitField, FieldList
+
+from config import config
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+DB_PASSWORD = config["DB_PASSWORD"]
+DB_NAME = config["DB_NAME"]
+DB_USER = config["DB_USER"]
+DB_CLUSTERNAME = config["DB_CLUSTERNAME"]
+DB_URI = "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+DB_CLUSTERNAME+".ovnng.mongodb.net/"+DB_NAME+"?retryWrites=true&w=majority"
+
+app.config["MONGO_URI"] = DB_URI
+# app.config["SECRET_KEY"] = config["WTF_SECRET_KEY"]
+
+mongo = PyMongo(app)
+
+# class LoginForm(FlaskForm):
+#     pathWidth = DecimalField('pathWidth')
+#     pathSegmentLength = DecimalField('segmentLength')
+#     imageUrls = StringField('imageUrls')
+
+
+
+# Function to validate URL 
+# using regular expression 
+def isValidURL(str):
+
+	# Regex to check valid URL 
+	regex = ("((http|https)://)(www.)?" +
+			"[a-zA-Z0-9@:%._\\+~#?&//=]" +
+			"{2,256}\\.[a-z]" +
+			"{2,6}\\b([-a-zA-Z0-9@:%" +
+			"._\\+~#?&//=]*)")
+	
+	# Compile the ReGex
+	p = re.compile(regex)
+
+	# If the string is empty 
+	# return false
+	if (str == None):
+		return False
+
+	# Return if the string 
+	# matched the ReGex
+	if(re.search(p, str)):
+		return True
+	else:
+		return False
+
+@app.route('/')
+def form():
+    # form = LoginForm()
+    # return render_template("form.html", form=form)
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])
+def form_post():
+    # get form input as dictionary
+    a = request.form.to_dict()
+    # extract image URIs
+    images = a["imageURI"].split(';')
+    image_list = []
+    # validate URIs and put them into list
+    for image in images:
+        if(isValidURL(image.strip())):
+            image_list.append(image.strip())
+    # replace imageURI string with image_list in input dictionary
+    a["imageURI"] = image_list
+    # generate custom ID for document
+    id  = mongo.db.config["METADATA_COLLNAME"].count_documents(filter={}) + 1
+    a["_id"] = id
+    # insert document
+    data = mongo.db.config["METADATA_COLLNAME"].insert_one(a)
+    # print the ID of the inserted document
+    return "your ID is: " + str(data.inserted_id)
+
+@app.route('/metadata', methods=['GET', 'POST', 'PUT'])
+def get_metadata():
+    if request.method == 'GET':
+        doc_id = int(request.args['id'])
+    elif request.method == 'POST'or 'POST':
+        doc_id = int(request.get_json()['id'])
+    # elif request.method == 'PUT':
+    #     # doc_id = int(request.json['id'])
+    #     print(request.get_json()['id'])
+    data = mongo.db.config["METADATA_COLLNAME"].find_one(filter={'_id' : doc_id})
+    return jsonify(data)
+    # return "done"
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
